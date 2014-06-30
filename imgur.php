a4c5c09cbad188e213b03a9dfaa890d0cd764343
Gildásio Jr. <gildasio97@gmail.com>
2014-06-30 04:36:27 -0300

    Create imgur.php
    The first version script.

---
+++ imgur.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+*
+* Script para Upload no imgur.com
+*
+* Esse script tem como função fazer o upload de uma imagem
+* E para isso ele usar o site imgur.com como hospedagem
+*
+* @author Gildásio Júnior
+* @link gildasio.net
+*
+**/
+
+function upload($img){
+	$ch = curl_init();
+	$data = array(
+		'Filedata' => "@$img"
+	);
+	curl_setopt($ch,CURLOPT_URL,'http://imgur.com/upload');
+	curl_setopt($ch,CURLOPT_POST,1);
+	curl_setopt($ch,CURLOPT_POSTFIELDS,$data);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+	$up = curl_exec($ch);
+	curl_close($ch);
+
+	preg_match("/\"success\":(.*?),\"status\"/i",$up,$ok);
+
+	if($ok[1] === 'true'){
+		preg_match("/hashes\":\[\"(.*?)\"\],\"hash/i",$up,$link);
+		$link = 'Link of image: http://imgur.com/'.$link[1];
+		return $link;
+	}
+	elseif($ok[1] === 'false'){
+		$erro = 'An error occurred, please try again later.';
+		$erro = $erro."\n";
+		$erro = $erro.'Or report in gildasio.net';
+		return $erro;
+	}
+	else{
+		$erro = 'An error occurred, please try again later.';
+		$erro = $erro."\n";
+		$erro = $erro.'Or report in gildasio.net';
+		return $erro;
+	}
+}
+
+if((count($argv) != 2) || !(is_file($argv[1])) || !(file_exists($argv[1]))){
+	echo "\nScript by Gildasio Junior";
+	echo "\nSee more in gildasio.net";
+	echo "\n\n";
+	echo "Use: php $argv[0] [an imagem location]";
+	echo "\n\n";
+	exit;
+}
+
+echo "\nScript by Gildasio Junior";
+echo "\nSee more in gildasio.net";
+echo "\n\n";
+echo upload($argv[1])."\n\n";
+
+?>
